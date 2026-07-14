70dfc6a66cefc96feb59df7d1b691739188da4ae
jiyansheejain3030 <jiyansheej@gmail.com>
2026-07-14 12:45:20 +0530

    embed debug

--- ai-gateway-platform/services/embedding_service/embed.py
+++ ai-gateway-platform/services/embedding_service/embed.py
@@ -27,13 +27,14 @@ def generate_embedding(prompt: str):
     # Return a dummy embedding
     return [0.0] * 384
 """
-from sentence_transformers import SentenceTransformer
+
 
 _model = None
 
 def get_model():
     global _model
     if _model is None:
+        from sentence_transformers import SentenceTransformer
         _model = SentenceTransformer("all-MiniLM-L6-v2")
     return _model
 
